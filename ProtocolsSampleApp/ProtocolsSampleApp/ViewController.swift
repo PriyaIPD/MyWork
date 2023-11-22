c33ee4f6ebecc517c1f1a8d22f78fc15950ec117
Priya2835 <priya_gandhi94@icloud.com>
2023-11-22 14:30:11 -0500

    Protocol Implementation

---
+++ ProtocolsSampleApp/ProtocolsSampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProtocolsSampleApp
+//
+//  Created by Priya Gandhi on 2023-11-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

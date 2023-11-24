8e417b12f1d457bb210c2c76bfc88a8e44ff9a06
Priya2835 <priya_gandhi94@icloud.com>
2023-11-24 10:27:28 -0500

    Higher order functions

--- HigherOrderDemo/HigherOrderDemo/ViewController.swift
+++ HigherOrderDemo/HigherOrderDemo/ViewController.swift
@@ -11,8 +11,41 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        performHigherOrderOperations(arr: [3,7,1,0,5,-1,2])
         // Do any additional setup after loading the view.
     }
+    
+    
+    func performHigherOrderOperations(arr :[ Int]){
+        //1. MAP function to multiple each value of array by 5
+        let arrMapped = arr.map({$0 * 5})
+        print("Map Operation Result is", arrMapped);
+        
+        //2. FIlter function to get even numbers only
+        let filteredArr = arr.filter({$0 % 2 == 0})
+        print("Filter Operation Result is", filteredArr);
+
+        //3. Sort function
+        let sortedArr = arr.sorted { a, b in
+            return a < b
+        }
+        print("Sort Operation Result is",sortedArr)
+
+        //4. Reduce function
+        let sum = arr.reduce(0, { x, y in
+            x + y
+        })
+        print("Reduce Operation Result is",sum)
+
+       //5, flatmap
+        let arrWithInArr = [[10,25,30],[45,50]]
+        let arrFlaten = arrWithInArr.flatMap { array in
+            return array
+        }
+        print(arrFlaten)
+    }
+    
+    
 
 
 }

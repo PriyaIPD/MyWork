9e6f65431fd704415d21206288078a3960babeab
“priya0510” <priya@getaround.com>
2025-04-05 08:38:31 -0400

    Initial Commit

---
+++ SpeerAssignment/SpeerAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeerAssignmentApp.swift
+//  SpeerAssignment
+//
+//  Created by Priya Gandhi on 2025-04-05.
+//
+
+import SwiftUI
+
+@main
+struct SpeerAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpeerAssignmentTests/SpeerAssignmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpeerAssignmentTests.swift
+//  SpeerAssignmentTests
+//
+//  Created by Priya Gandhi on 2025-04-05.
+//
+
+import Testing
+@testable import SpeerAssignment
+
+struct SpeerAssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
